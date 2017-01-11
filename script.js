d12e18ba737c686fed6e02efbdbaaf4834fb66e0
Stephanie <stephykins@gmail.com>
2017-01-11 14:26:21 -0500

    set up html and css and initialize script file

---
+++ script.js
@@ -0,0 +1,11 @@
+//allow user to select a date
+//take that date and store it with the name of the event in an object
+//store that object or array in local storage
+//ability to set background
+
+//timer
+//store the event date
+//store the date now
+//use set interval to update the days left
+//when the current date and the event date match up end the timer and display 0 days left
+//days left hours left minutes left seconds left
