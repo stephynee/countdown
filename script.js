aed935e01d7aa0856ee37d4745dd2c58ee172ef4
Stephanie <stephykins@gmail.com>
2017-01-11 15:22:04 -0500

    create toggle for add event divs

--- script.js
+++ script.js
@@ -9,3 +9,22 @@
 //use set interval to update the days left
 //when the current date and the event date match up end the timer and display 0 days left
 //days left hours left minutes left seconds left
+
+(function() {
+  var plusButton = document.querySelector('#add span'),
+      eventDiv = document.querySelector('#add > div:nth-child(2)'),
+      dateDiv = document.querySelector('#add > div:nth-child(3)')
+      addButton = document.querySelector('#add button');
+
+  function toggleAddEvent() {
+    eventDiv.style.display = isHidden(eventDiv) === 'none' ? 'block' : 'none';
+    dateDiv.style.display = isHidden(dateDiv) === 'none' ? 'block' : 'none';
+    addButton.style.display = isHidden(addButton) === 'none' ? 'block' : 'none';
+  }
+
+  function isHidden(el) {
+    return window.getComputedStyle(el, null).getPropertyValue('display')
+  }
+
+  plusButton.addEventListener('click', toggleAddEvent);
+}());
